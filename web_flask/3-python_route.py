4dc91d159231a0fff486a8b5a07c0cf6db6e09ee
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-06-04 21:28:29 -0700

    p

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """return hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Returns 'C ' followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text = 'is cool'):
+    """Returns 'C ' followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
