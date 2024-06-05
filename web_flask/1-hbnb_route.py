f72aa52d1fc155dd37395509870fcad30c5ad258
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-06-04 20:57:24 -0700

    p

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,9 +3,9 @@
 starts a Flask web application
 """
 
+
 from flask import Flask
 app = Flask(__name__)
-
 
 
 @app.route('/', strict_slashes=False)
@@ -13,10 +13,11 @@ def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
-	"""return hbnb"""
-	return 'HBNB'
+    """return hbnb"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
