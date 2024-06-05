039e7046405e7c0ee24154171619ebf99db6865d
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-06-04 21:15:08 -0700

    p

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """return hbnb"""
+    return 'HBNB'
+
+
+@app.route('/C/<text>', strict_slashes=False)
+def show_c(text):
+    """Returns 'C ' followed by the value of the text variable"""
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
