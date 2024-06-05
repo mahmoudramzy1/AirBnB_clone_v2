89f838154fa6aab4e30fa607b46f5ba48d32d636
root <mahmoud.ahmed.ramzy@outlook.com>
2024-06-05 04:22:35 +0000

    p

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,7 +23,9 @@ def hbnb():
 @app.route('/C/<text>', strict_slashes=False)
 def show_c(text):
     """Returns 'C ' followed by the value of the text variable"""
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+	text = text.replace('_', ' ')
+	return 'C {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
