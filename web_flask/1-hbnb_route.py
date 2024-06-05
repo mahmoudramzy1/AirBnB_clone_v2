a581a6b37318a98afe5669cdb9ef38ff320efc4a
root <mahmoud.ahmed.ramzy@outlook.com>
2024-06-05 03:54:20 +0000

    p

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,5 +13,11 @@ def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+@app.route('/hbnb')
+def hbnb():
+	"""return hbnb"""
+	return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
