c746dfa15d38eede2557923b2954964d13f96e4e
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-06-04 21:31:01 -0700

    p

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def c_text(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-
+@app.route('/python/')
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text = 'is cool'):
     """Returns 'C ' followed by the value of the text variable"""
